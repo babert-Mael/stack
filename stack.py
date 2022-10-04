96e9a1b5d194f394a94d61c119a5fc9c33415fc7
Maël BABERT <m.babert@workstation2.nsi.lan>
2022-10-04 13:42:58 +0200

    début du projet stack

---
+++ stack.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Stack:
+    """An implementation of a Stack"""
+    
+    def __init__(self):
+        raise NotImplementedError
+    
+    def isEmpty(self):
+        """
+        Return True if the stack is empty, False otherwise
+        """
+        raise NotImplementedError
+    
+    def push(self, element):
+        """
+        Push an element into the stack
+        return the stack
+        """
+
+    def pop(self):
+        """
+        pop an element from the stack
+        return the poped element
+        """
+        raise NotImplementedError
+
+    def summit(self):
+        """
+        red the top element of the stack without removing it.
+        return the top element.
+        """
+        raise NotImplementedError
+
+    def size(self):
+        """
+        read the size of the stack
+        return the size as an integer.
+        """
+        raise NotImplementedError
+        
+        
---
+++ test.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+from stack import Stack
+
+class StackTests(unittest.TestCase):
+    """
+    A class to test the Stack module
+    """
+    def setUp(self):
+        """
+        create an empty Stack for all tests
+        """
+        self.stack=Stack()
+    
+    def tearDown(self):
+        """
+        call this at the end of each tests, wheter the test fail or not
+        """
+        self.stack=Stack()
+
+    def testPush(self):
+        """
+        Check if the push method works
+        """
+        element='element'
+        mystack=self.stack.push(element)
+        self.assertTrue(isinstance(mystack, Stack), "The push method should return an instance of Stack")
+        
+        
+    def testPop(self):
+        """
+        check if the pop method works
+        """
+        elements=['How','are','you','today','?']
+        
+        for e in elements:
+            self.stack.push(e)
+        pops=[self.stack.pop() for e in elements]
+        reverse=elements[::-1] #we reverse the input list
+        self.assertEqual(reverse, pops, "pops elements should be %s, but we have %s instead."%(reverse, pops))
+        with self.assertRaises(IndexError):
+            self.stack.pop()
+
+    def testIsEmpty(self):
+        """
+        Check if the isEmpty method works
+        """
+        answer1=self.stack.isEmpty()
+        self.assertEqual(answer1, True, "isEmpty should be True, it returned %s instead."%answer1)
+        element='element'
+        self.stack.push(element)
+        answer2=self.stack.isEmpty()
+        self.assertEqual(answer2, False, "isEmpty should be False, it returned %s instead."%answer2)
+        
+    def testSize(self):
+        """
+        Check if the size method works
+        """
+        size1=self.stack.size()
+        self.assertEqual(size1, 0, "size should be 0, it returned %s instead."%size1)
+        self.stack.push('test')
+        size2=self.stack.size()
+        self.assertEqual(size2, 1, "size should be 1, it returned %s instead."%size2)
+        self.stack.pop()
+        size3=self.stack.size()
+        self.assertEqual(size3, 0, "size should be 0, it returned %s instead."%size3)
+        
+    def testSummit(self):
+        """
+        Check if the summit method works
+        """
+        elements=['How','are','you','today','?']
+        
+        for e in elements:
+            self.stack.push(e)
+        summit1=self.stack.summit()
+        self.assertEqual(elements[-1], summit1, "summit should be %s, it returned %s instead."%(elements[-1], summit1))
+        self.stack.pop()
+        summit2=self.stack.summit()
+        self.assertEqual(elements[-2], summit2, "summit should be %s, it returned %s instead."%(elements[-2], summit2))
+        self.stack=Stack()
+        with self.assertRaises(IndexError):
+            self.stack.summit()
+        
+if __name__ == '__main__':
+    unittest.main()        
