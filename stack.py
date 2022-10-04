5e8444c04082f0ab35721d26c75c9392d122c39a
Maël BABERT <m.babert@workstation2.nsi.lan>
2022-10-04 14:08:49 +0200

    implémentation de toutes les fonction

--- stack.py
+++ stack.py
@@ -5,39 +5,44 @@ class Stack:
     """An implementation of a Stack"""
     
     def __init__(self):
-        raise NotImplementedError
+        self._element=[]
+
     
     def isEmpty(self):
         """
         Return True if the stack is empty, False otherwise
         """
-        raise NotImplementedError
+        return self._element==[]
     
     def push(self, element):
         """
         Push an element into the stack
         return the stack
         """
-
+        self._element.append(element)
+        return self
+    
     def pop(self):
         """
         pop an element from the stack
         return the poped element
         """
-        raise NotImplementedError
+        pop_element=self._element[-1]
+        self._element=self._element[:-1]
+        return pop_element
 
     def summit(self):
         """
         red the top element of the stack without removing it.
         return the top element.
         """
-        raise NotImplementedError
+        return self._element[-1]
 
     def size(self):
         """
         read the size of the stack
         return the size as an integer.
         """
-        raise NotImplementedError
+        return len(self._element)
         
         
