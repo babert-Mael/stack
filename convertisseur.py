726f5981dd5d05afa4ed4709beea110bb403568e
Maël BABERT <m.babert@workstation3.nsi.lan>
2022-10-11 11:12:13 +0200

    test_convertisseur.pyfin du convertisseur, il marche

--- convertisseur.py
+++ convertisseur.py
@@ -1,13 +1,27 @@
 from stack import Stack
 
-def convertisseur(op):
-pile=Stack()
-operateur={'+':+,'-':-,'*':*}
-    for l in op:
-        resultat=0
-        pile.push(l)
-        if l in operateur:
-            for i in range (2):
-                pile.pop()
-            
-            
+def calcul(operation):
+    """
+    une fonction qui permet de faire de calcul en ecriture polonaise inverse
+    operation étant une chaine de caractère
+    """
+    
+    pile=Stack()
+    operateur=('+', '-', '*', '^')
+    for c in operation:
+      resultat=0
+      if c not in operateur: # si c n'est pas un operateur
+          pile.push(c) # met la valeur dans la pile
+      else:
+        op1=int(pile.pop()) # op1 est égale à la valeur en haut de la pile
+        op2=int(pile.pop()) # op2 est égale à la valeur en bas de la pile
+        if c=='*': # si le caracter est égale à * fait une multiplication des opérateur
+          resultat=op2 * op1
+        elif c=='+': # si le caractere est égale à + fait un addition des deux multiplicateur
+          resultat= op2 + op1
+        elif c=='-': #si le caractere est égale à - fait une soustraction des deux opérateurs
+          resultat=op2-op1
+        elif c=='^': #si le caractere est égale à ^ fait une puissance des deux opérateurs
+            resultat=op2**op1  
+        pile.push(resultat)
+    return pile.pop()
---
+++ test_convertisseur.py
@@ -0,0 +1,12 @@
+import unittest
+from convertisseur import calcul
+class Convertest(unittest.TestCase):
+    
+    def testConver1(self): #on fait un test
+        expressions=[("2","4","3","+","*","4","-"), ("5","4","+","2","9","*","4","-","+"), ("11", "45", "8", "6", "-", "+","+","2","^"), ("-2", "-4","+"), ("4.5","5","+")]
+        resultats=(10, 23, 3364, -6, 9.5)
+        for i in range(len(expressions)-1):
+            self.assertEqual(calcul(expressions[i]), resultats[i]," l'expression %s est égale à %s, elle devrait être égale à %s"%(expressions[i],calcul(expressions[i]), resultats[i]))
+            
+if __name__ == '__main__':
+    unittest.main()
