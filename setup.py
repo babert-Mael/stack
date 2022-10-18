412f5b92c1b88eed831be0ff795e01331c07eb67
Maël BABERT <m.babert@workstation3.nsi.lan>
2022-10-18 14:09:25 +0200

    creation du setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Stack',
+    version='0.1',
+    url='https://github.com/babert-Mael/stack',
+    author='Babert Maël',
+    author_email='babert-m@saint-louis29.net',
+    description='A program to make stack in python',
+    packages=find_packages(),   
+    install_requires=[],
+)
